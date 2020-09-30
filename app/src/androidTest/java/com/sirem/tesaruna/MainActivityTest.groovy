d1a8f2c8ec3d63cb2be667f4cc7061282757e7be
tony rafi <toni@pengembangan.lakuin.id>
2020-09-30 15:07:51 +0700

    unit tes

---
+++ app/src/androidTest/java/com/sirem/tesaruna/MainActivityTest.groovy
@@ -0,0 +1,30 @@
+package com.sirem.tesaruna
+
+class MainActivityTest extends GroovyTestCase {
+    void testGetViewmodelProviderFactory() {
+    }
+
+    void testSetViewmodelProviderFactory() {
+    }
+
+    void testGetViewModel() {
+    }
+
+    void testSetViewModel() {
+    }
+
+    void testGetAdapter$app_debug() {
+    }
+
+    void testSetAdapter$app_debug() {
+    }
+
+    void testOnCreate() {
+    }
+
+    void testGetDataLocal() {
+    }
+
+    void testGetDataApi() {
+    }
+}
---
+++ app/src/androidTest/java/com/sirem/tesaruna/model/DataViewModelTest.groovy
@@ -0,0 +1,24 @@
+package com.sirem.tesaruna.model
+
+class DataViewModelTest extends GroovyTestCase {
+    void testGetLiveDataPostApi() {
+    }
+
+    void testGetDataPost() {
+    }
+
+    void testInsertDataAll() {
+    }
+
+    void testGetLiveDataLocal() {
+    }
+
+    void testGetLisDataLocal() {
+    }
+
+    void testGetApplication() {
+    }
+
+    void testGetDataRepository() {
+    }
+}
